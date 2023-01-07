1dac4f6c57cc09416a00ea534d29196b7d6ed4c3
Smruti ranjan Nayak <srnayak165@gmail.com>
2023-01-07 23:45:16 +0530

    2 programs added

---
+++ KeywordCheck.java
@@ -0,0 +1,6 @@
+public class KeywordCheck {
+	public static void main(String ...args) {
+		int _ = 12;
+		System.out.println(_); 
+	}
+}
---
+++ StaticImport.java
@@ -0,0 +1,9 @@
+import static java.lang.Integer.*;
+
+public class StaticImport {
+	public static void main(String ...args) {
+		String string = "121";
+		int number = parseInt(string);
+		int maxRange = MAX_VALUE;
+	}
+}
